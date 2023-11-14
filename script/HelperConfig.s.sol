b8665bfa95c91bb4e568ce8ebc8bb103f14b5e56
aciDrums7 <edoardo.carradori.96@gmail.com>
2023-11-14 13:00:35 +0100

    Magic Numbers

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -15,6 +15,9 @@ contract HelperConfig is Script {
     // Otherwise, grap the existing address from the live network
     NetworkConfig public activeNetworkConfig;
 
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
+
     struct NetworkConfig {
         address priceFeed; // ETH/USD price feed address
     }
@@ -52,7 +55,10 @@ contract HelperConfig is Script {
         //2) Return the mock address
 
         vm.startBroadcast();
-        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(8, 2000e8);
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            INITIAL_PRICE
+        );
         vm.stopBroadcast();
         NetworkConfig memory anvilConfig = NetworkConfig({
             priceFeed: address(mockPriceFeed)
