abcc7a96ad4f5ac910ffda18bd3376a5571ae18e
aciDrums7 <edoardo.carradori.96@gmail.com>
2023-11-14 17:40:23 +0100

    More Cheatcodes

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -8,11 +8,14 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 contract FundMeTest is Test {
     FundMe fundMe;
 
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether; // 1e17 Wei
+    uint256 constant STARTING_BALANCE = 10 ether;
+
     function setUp() external {
-        //? us -> FundMeTest -> FundMe
-        // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE);
     }
 
     function testMinimumDollarIsFive() public {
@@ -29,5 +32,19 @@ contract FundMeTest is Test {
     function testPriceFeedVersionIsAccurate() public {
         uint256 version = fundMe.getVersion();
         assertEq(version, 4);
+    }
+
+    function testFundFailsWithoutEnoughEth() public {
+        vm.expectRevert(); // next line should revert
+        fundMe.fund(); // send 0 value
+    }
+
+    function testFundUpdatedFundedDataStructures() public {
+        vm.prank(USER); // The next TX will be sent by USER
+
+        fundMe.fund{value: SEND_VALUE}();
+
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
     }
 }
